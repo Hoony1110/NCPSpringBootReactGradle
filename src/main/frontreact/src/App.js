dcf8f274f6901694fbf77aaeec09cd8db35769d8
JH <wkdgns9797@naver.com>
2024-11-15 15:13:31 +0900

    1

---
+++ src/main/frontreact/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Spring Boot + React + NCP</h1>
+      <h3>카운트 : 2</h3>
+      <img src="./image/rr.jpg" alt="샘플"/>
+
+    </div>
+  );
+};
+
+export default App;
